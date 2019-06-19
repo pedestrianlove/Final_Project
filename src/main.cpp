03866eb6f175092e099cab1b165464de607491f8
pedestrianlove <j.s.li@gapp.nthu.edu.tw>
2019-06-19 17:59:34 +0800

    added OSX magic main functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <stdio.h>
-
+#include <allegro5/allegro.h>
 #include "music.h"
 
-
-int main ()
+int real_main (int argc, char **argv)
 {
 	HELLO ();
 
 
+
 	return 0;
+}
+
+
+
+int main(int argc, char **argv)
+{
+	return al_run_main(argc, argv, real_main);
 }
